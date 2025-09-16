0e250c0745a1403ec4132f5cdd75c753a1a563cc
ds4531kumar <ds4531>
2025-09-16 21:29:26 +0530

    Add Calculator

---
+++ TestProject/TestMethod/Calculator.cs
@@ -0,0 +1,15 @@
+﻿namespace TestMethod
+{
+    public static class Calculator
+    {
+        public static int Add(int a, int b) => a + b;
+
+        public static int Subtract(int a, int b) => a - b;
+
+        public static int Divide(int a, int b)
+        {
+            if (b == 0) throw new DivideByZeroException();
+            return a / b;
+        }
+    }
+}
